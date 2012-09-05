d21a603338006d64a5a4ecfd94972f7893ca04c6
Flemming Hoffmeyer <flemming.b.h@gmail.com>
2012-09-05 19:31:38 +0200

    animation stuff in jquery

--- js/script.js
+++ js/script.js
@@ -3,10 +3,12 @@ var delay = 8;
 var interval;
 $(document).ready(function(){
   loadBulletholes();
-  $("body").click(function(){bullethole_counter = 0; $(".bullethole").remove();});
+  fade_in_wanted_poster();
+  init_info_boxes();
 });
 
 function loadBulletholes(){
+  $("body").click(function(){bullethole_counter = 0; $(".bullethole").remove();});
   interval = setInterval(drawBullethole, delay*1000);
 }
 
@@ -15,4 +17,33 @@ function drawBullethole(x, y){
     var y = Math.floor(Math.random()*$(window).height()+$("body").scrollTop()+1);
     $("body").append("<div class='bullethole' style='left: " + x + "px; top: " + y + "px;'><embed src='images/GunShot.mp3' hidden=true autostart=true loop=false></div>");
     bullethole_counter++;
+}
+
+function fade_in_wanted_poster(){
+  var delay = 1000;
+  $(".fade_in").each(function(index){
+    $(this).delay(delay).queue(function(n){
+      $(this).animate({opacity: 1}, 1000);
+      n();
+    });
+    delay += 3000;
+  });
+}
+
+function init_info_boxes(){
+  var screen_height = $(window).height();
+
+  $(document).scroll(function() {
+
+    var screen_center = $(document).scrollTop() + screen_height/2;
+
+    $(".infobox").each(function(){
+      var infobox_top = $(this).position().top;
+      var amount_shown =  ($(document).scrollTop() + screen_height) - infobox_top;
+      if(amount_shown > 0){
+        var opacity_pct = amount_shown/screen_height;
+      }
+      $(this).css('opacity',opacity_pct);
+    });
+  });
 }
